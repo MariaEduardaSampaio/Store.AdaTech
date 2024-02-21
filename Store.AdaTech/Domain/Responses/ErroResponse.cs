58ce2c6a95af544a2b80c94008674aec3e234710
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-21 09:49:50 -0300

    cria classe de erroResponse para tratar exceção

---
+++ Store.AdaTech/Domain/Responses/ErroResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.AdaTech.Domain.Responses
+{
+    public class ErroResponse
+    {
+        public ErroResponse(string detalhes, int statusCode)
+        {
+            Detalhes = detalhes;
+            StatusCode = statusCode;
+        }
+
+        public string Detalhes { get; set; }
+        public int StatusCode { get; set; }
+
+    }
+}
