49ae52fbfcabf5bac595e0c775ceeb0d3d0bda8d
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-21 09:48:47 -0300

    feat: cria filtro de tratar exceções

---
+++ Store.AdaTech/Application/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Store.AdaTech.Domain.Responses;
+
+namespace Store.AdaTech.Application.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = 400;
+            var erroResult = new ErroResponse(context.Exception.Message, context.HttpContext.Response.StatusCode);
+            var result = new JsonResult(erroResult);
+
+            context.Result = result;
+        }
+    }
+}
