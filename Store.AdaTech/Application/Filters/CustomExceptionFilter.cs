4d9507bd6176ea9d34a9246a5523e27625a63c84
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-21 12:01:06 -0300

    adiciona log de erro no filter

--- Store.AdaTech/Application/Filters/CustomExceptionFilter.cs
+++ Store.AdaTech/Application/Filters/CustomExceptionFilter.cs
@@ -6,12 +6,20 @@ namespace Store.AdaTech.Application.Filters
 {
     public class CustomExceptionFilter : IExceptionFilter
     {
+        private readonly ILogger<CustomExceptionFilter> _logger; 
+
+        public CustomExceptionFilter(ILogger<CustomExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnException(ExceptionContext context)
         {
+            _logger.LogError(context.Exception, "Ocorreu uma exceção.");
+
             context.HttpContext.Response.StatusCode = 400;
             var erroResult = new ErroResponse(context.Exception.Message, context.HttpContext.Response.StatusCode);
             var result = new JsonResult(erroResult);
-
             context.Result = result;
         }
     }
